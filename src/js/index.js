837a7a4c8efe4c5d943be1295d479ee100debe2c
YanChorniiOSF <yan.chornii@osf-commerce.com>
2018-12-03 17:40:06 +0200

    Урок 130. How ES6 Modules Work Урок 131. Making our First API Calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,12 @@
-const css = require('../app.scss')
-// Global app controller
+const css = require('../app.scss');
+import axios from 'axios';
+
+async function getResults(query) {
+    const key = '0799e331f50a9e75b2d84b44decc706c';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);   
+    } catch {
+        alert (error);
+    }
+}
+getResults('pizza');
