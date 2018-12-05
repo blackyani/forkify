2d559c1af36b6a64c6161b97b1fe88f141f1fcb6
YanChorniiOSF <yan.chornii@osf-commerce.com>
2018-12-05 14:25:03 +0200

    Урок 134. Building the Search View - Part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 const css = require('../app.scss');
 import Search from './models/Search';
+import * as seachViev from './views/searchView';
+import {elements} from './views/base';
+
 
 /* Global state of the app
 * - Seach object
@@ -11,23 +14,25 @@ const state = {};
 
 const controlSearch = async () => {
     // 1)Get query from view
-    const query = 'pizza' //TODO
-
+    const query = seachViev.getInput();
+    
     if (query) {
         // New seach object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
+        seachViev.clearInput();
+        seachViev.clearResults();
 
         // 4) Seach for recipes
         await state.search.getResults();
 
         //5) render results on UI
-        console.log(state.search.result);
+        seachViev.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
