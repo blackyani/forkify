fce69201a03de5f4ea5c21d4e2470628dc2797ee
YanChorniiOSF <yan.chornii@osf-commerce.com>
2018-12-05 10:06:08 +0200

    Урок 133. Building the Search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,33 @@
 const css = require('../app.scss');
 import Search from './models/Search';
 
-const search = new Search('pizza') ;
-console.log();
-search.getResults();
+/* Global state of the app
+* - Seach object
+* - Current recipe object
+* - Shoppeng list obj
+* - Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1)Get query from view
+    const query = 'pizza' //TODO
+
+    if (query) {
+        // New seach object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Seach for recipes
+        await state.search.getResults();
+
+        //5) render results on UI
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
